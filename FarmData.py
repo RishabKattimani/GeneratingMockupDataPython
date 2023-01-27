9d8cf03ef5148a36cdd659f5f52955631cbe7fb8
RishabKattimani <63938552+RishabKattimani@users.noreply.github.com>
2023-01-26 20:49:12 -0600

    Update FarmData.py

--- FarmData.py
+++ FarmData.py
@@ -6,11 +6,19 @@ import csv
 import FarmDataConfig as config
 
 TotalRecords=10
+fake=Fake()
+if (len(sys.argv)>2):
+    print("Usage: FarmData <OutputFileName>")
+else:
+    with open(sys.argv[1], 'w', newline='\n') as file:
+        writer = csv.writer(file)
+        writer.writerow(config.ColumnHeaders)
 
-print(
-fake.Name.name(),
-random.choice(config.LiveStockType),
-random.randrange(1, 200),
-config.random_date("1/1/2019 1:34 PM", "9/12/2020 1:34 PM"),
-str(random.randrange(1000, 100000))
-)
+        for i in range(1, TotalRecords+1):
+            writer.writerow([
+            fake.Name.name(),
+            random.choice(config.LiveStockType),
+            random.randrange(1, 200),
+            config.random_date("1/1/2019 1:34 PM", "9/12/2020 1:34 PM"),
+            str(random.randrange(1000, 100000))
+            ])
